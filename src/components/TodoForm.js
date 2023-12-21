6a7e8e5c6a24837a7e76f02fec9fca69e139cb58
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-12-20 19:10:19 -0800

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    const[value, setValue] = useState("")
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(value)
+    }
+
     return(
         // instead of div it will be form
-        <form className = 'TodoForm'>TodoForm
-            <input type = "text" className = 'todo-input' placeholder = 'New Todo'/>
-            <button type = 'submit' className = 'todo-btn'></button>
+        <form className = 'TodoForm' onSubmit = {handleSubmit}>TodoForm
+            <input type = "text" className = 'todo-input' placeholder = 'New Todo' onChange = {(event) => console.log(event.target.value)}/>
+            <button type = 'submit' className = 'todo-btn'>Add new Task</button>
         </form>
     )
 }

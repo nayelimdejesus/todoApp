85f3da208aae8f855c625fddd36036f111a310e5
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-12-20 21:02:16 -0800

    Created toggleComplete function

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { TodoForm } from './TodoForm';
+import { Todo } from './Todo';
 import {v4 as uuidv4} from 'uuid';
 uuidv4();
 
@@ -9,14 +10,19 @@ export const TodoWrapper = () => {
         setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
         console.log(todos)
     }
+    const toggleComplete=id => {
+        setTodos(todos.map(todo => todo.id === id ? 
+            {...todo, completed: !todo.completed} : todo))
+    }
     return(
-        <div className = 'TodoWrapper'>
+        <div className= 'TodoWrapper'>
+            <h1> ToDo List</h1>
             {/* import TodoForm and Todo*/}
-            <TodoForm addTodo = {addTodo}/>
+            <TodoForm addTodo={addTodo}/>
             {todos.map((todo, index) =>(
-                <Todo task = {todo} key = {index}/>
+                <Todo task={todo} key={index} toggleComplete={toggleComplete}/>
             ))}
-            <Todo/>
+        
         </div>
     )
 }

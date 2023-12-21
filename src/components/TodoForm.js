acfe7b23602d9dcff2c0d340da485fd58b2236c0
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-12-20 18:40:54 -0800

    Creating form in TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,9 @@ import React from 'react'
 export const TodoForm = () => {
     return(
         // instead of div it will be form
-        <form className = 'TodoForm'>TodoForm</form>
+        <form className = 'TodoForm'>TodoForm
+            <input type = "text" className = 'todo-input' placeholder = 'New Todo'/>
+            <button type = 'submit' className = 'todo-btn'></button>
+        </form>
     )
 }
